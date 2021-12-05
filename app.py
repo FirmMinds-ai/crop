2f37aa102cdb49108a76df1817fad917c09dd9d4
FirmMinds-ai <87647533+FirmMinds-ai@users.noreply.github.com>
2021-12-05 09:39:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 from flask import Flask, flash, request
 from werkzeug.utils import secure_filename
+import matplotlib.pyplot as plt
 
 UPLOAD_FOLDER = 'uploads'
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -25,6 +26,9 @@ def upload_file():
 
         path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(path)
+        img = plt.imread(path)
+        print(type(img))
+        print(img.shape)
         return "success"
 
 if __name__ == '__main__':
